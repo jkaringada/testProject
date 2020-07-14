2d5b10da8f7b61337cdc7c0481bbbf757093a821
Karingada, Jeff <jeff.karingada@fisglobal.com>
2020-07-14 11:35:10 +1000

    resource

---
+++ src/main/java/testProject/Testbug.java
@@ -0,0 +1,25 @@
+package testProject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.nio.charset.StandardCharsets;
+
+public class Testbug {
+	
+	public void TestUrl() throws MalformedURLException, IOException {
+		String str = "";
+	    URLConnection conn 
+	      = new URL("http://norvig.com/big.txt").openConnection();
+	    BufferedReader br = new BufferedReader(
+	      new InputStreamReader(conn.getInputStream(), StandardCharsets.UTF_8));
+	    
+	    while (br.readLine() != null) {
+	        str += br.readLine();
+	    } 
+	}
+
+}
