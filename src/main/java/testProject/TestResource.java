08197e944670e9cd914460ac37b5a45566fbea07
Karingada, Jeff <jeff.karingada@fisglobal.com>
2020-07-14 11:18:26 +1000

    first commit

---
+++ src/main/java/testProject/TestResource.java
@@ -0,0 +1,12 @@
+package testProject;
+
+public class TestResource {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("hello");
+
+	}
+
+}
