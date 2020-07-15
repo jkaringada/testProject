132b996d1acb0b45e6e99bfafb1d2ee98cc1f5bd
Karingada, Jeff <jeff.karingada@fisglobal.com>
2020-07-15 11:14:37 +1000

    resource3

--- src/main/java/testProject/ResourceUrl.java
+++ src/main/java/testProject/ResourceUrl.java
@@ -1,5 +1,25 @@
 package testProject;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.nio.charset.StandardCharsets;
+
 public class ResourceUrl {
+	
+	public void TestUrl() throws MalformedURLException, IOException {
+		String str = "";
+	    URLConnection conn 
+	      = new URL("http://norvig.com/big.txt").openConnection();
+	    BufferedReader br = new BufferedReader(
+	      new InputStreamReader(conn.getInputStream(), StandardCharsets.UTF_8));
+	    
+	    while (br.readLine() != null) {
+	        str += br.readLine();
+	    } 
+	}
 
 }
---
+++ src/main/java/testProject/TestConn.java
@@ -0,0 +1,32 @@
+package testProject;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class TestConn {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		String someFile="jeff.txt";
+
+		FileInputStream localFileInputStream = null;
+		try {
+			localFileInputStream = new FileInputStream(someFile);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		InputStreamReader localInputStreamReader = new InputStreamReader(localFileInputStream);
+		BufferedReader reader = new BufferedReader(localInputStreamReader);
+		
+		 String line = null;
+		    while((line = reader.readLine()) != null) {
+		      String formattedLine = (line);
+		      // do other stuff
+		    }
+	}
+
+}
